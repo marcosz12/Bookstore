53d4fd535e8e5f940d118971d0e236b255043001
Marcos <vimarcos188@gmail.com>
2024-11-12 16:10:08 -0300

    puxando do pc do senac

--- Models/Genre.cs
+++ Models/Genre.cs
@@ -6,6 +6,7 @@ namespace BookStore.Models
     {
         public int Id { get; set; }
         [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string Name { get; set; }
         //public ICollection<Book> Books { get; set; } = new List<Book>();
         public Genre() { }
